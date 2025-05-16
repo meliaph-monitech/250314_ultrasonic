19a7b1847878e56d0b75dd27641463fbd1832364
meliaph-monitech <meliaph.monitech@gmail.com>
2025-05-16 13:44:27 +0900

    Update work_250516_UltraSDI_V05_AdjustFigSize.py

--- work_250516_UltraSDI_V05_AdjustFigSize.py
+++ work_250516_UltraSDI_V05_AdjustFigSize.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 7})
 import scipy.signal as signal
 import zipfile
 import os
